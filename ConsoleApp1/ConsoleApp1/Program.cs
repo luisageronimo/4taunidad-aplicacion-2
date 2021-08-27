9fc3cadfe0dc48598930a69fa1862fd146c0b1a5
Luisa Gerónimo <luisageronimo5@gmail.com>
2021-08-26 18:22:21 -0600

    completado

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Aplicación2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char op = 's';
+            string archivo = "";
+            int OP;
+
+            while (op == 's')
+            {
+                Console.WriteLine("Que desea hacer: \n1. Crear Archivo \n2. Escribir Archivo \n3. Visualizar contenido \n4. Salir ");
+                OP = int.Parse(Console.ReadLine());
+                string nombre;
+
+                Console.Clear();
+                if (OP == 1)
+                {
+                    StreamWriter ar;
+                    Console.WriteLine("Que nombre le pondrá: ");
+                    archivo = Console.ReadLine();
+                    ar = File.CreateText(archivo + ".txt");
+                    nombre = archivo;
+
+                    ar.Close();
+                }
+                if (OP == 2)
+                {
+                    EscribirArchivo(archivo);
+                }
+                if (OP == 3)
+                {
+                    MostrarContenido(archivo);
+                }
+
+                Console.ReadKey();
+                if (OP == 4)
+                {
+                    Console.WriteLine("Deseo salir");
+                }
+                Console.Clear();
+                Console.WriteLine("Desea Continuar ['s/n']");
+                op = char.Parse(Console.ReadLine());
+
+            }
+        }
+        static void EscribirArchivo(string nombre)
+        {
+            StreamWriter ar;
+            Console.WriteLine("Que escribira: ");
+            ar = File.AppendText(nombre);
+            ar.WriteLine(Console.ReadLine());
+            ar.Close();
+        }
+        static void MostrarContenido(string ruta)
+        {
+            StreamReader ar;
+            ar = File.OpenText(ruta);
+            Console.WriteLine(ar.ReadToEnd());
+        }
+    }
+}
